8b0d5229df7acd13e581f015f552d75b39407a81
vipin <pvipin907@gmail.com>
2021-07-10 00:34:44 +0530

    login and Data layer setup

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import Login from './Login'
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player'
+import { useDataLayerValue } from './DataLayer';
+
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+
+  const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token)
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
+      })
+    }
+    console.log("i am toekn", token)
+  }, [])
+  console.log("person", user);
+  return (
+    <div className="app">
+      {token ? <Player /> : (<Login />)}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/spotify.js
@@ -0,0 +1,23 @@
+export const authEndpoint = "https://accounts.spotify.com/en/";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "99c0c356e2f14ef2b8ef1fc58c45b415"
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash.substring(1).split('&').reduce((initial, item) => {
+
+        let parts = item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+        return initial
+    }, {});
+}
+export const loginUrl = `${authEndpoint}authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
